a34454933e8a716557dca091ddee852307ef056b
leafsummer <wing.sun1987@gmail.com>
2016-01-11 12:07:38 +0800

    update the file

--- updateddnsmasq.sh
+++ updateddnsmasq.sh
@@ -4,8 +4,7 @@ cnlist() {
     wget -4 --no-check-certificate -O /etc/dnsmasq.d/bogus-nxdomain.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
 }         
 adblock() {                                                                                                                                                             
-    wget -4 --no-check-certificate -O - https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt |                                                     
-    grep ^\|\|[^\*]*\^$ |                                                                                                                                           
+    wget -4 --no-check-certificate -O - https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ |                                                                                                                                           
     sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | uniq > /etc/dnsmasq.d/adblock.conf                                                                        
 
     wget -4 --no-check-certificate -O - https://raw.githubusercontent.com/kcschan/AdditionalAdblock/master/list.txt |                                               
