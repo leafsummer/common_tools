3ad04cca9ee9e0ef4786f946b90a92ea7d7d2ee4
leafsummer <wing.sun1987@gmail.com>
2016-02-24 03:30:08 +0800

    update

--- prime.py
+++ prime.py
@@ -36,51 +36,51 @@ import time
 
 # i=2
 # while i<a:
-        # if not list_1[i]:
-                # j = i * i
-                # if j > a:
-                        # break
-                # if i == 2:
-                        # while j<a:
-                                # list_1[j] = True
-                                # j += i
-                # else:
-                        # while j<a:
-                                # list_1[j] = True
-                                # j = j + i + i
+# if not list_1[i]:
+    # j = i * i
+    # if j > a:
+        # break
+    # if i == 2:
+        # while j<a:
+            # list_1[j] = True
+            # j += i
+    # else:
+        # while j<a:
+            # list_1[j] = True
+            # j = j + i + i
 
 # print sum(filter(lambda x:not x, list_1))
 
 if __name__ == "__main__":
     n = 4000000
-        start = time.time()
-        if n < 2:
-            print []
-        if n == 2:
-            print [2]
-        s = range(3, n, 2)
-        mroot = n ** 0.5
-        half = len(s)
+    start = time.time()
+    if n < 2:
+        print []
+    if n == 2:
+        print [2]
+    s = range(3, n, 2)
+    mroot = n ** 0.5
+    half = len(s)
 
-        i = 0
-        m = 3
-        # print 's:', s
-        while m <= mroot:
-            if s[i]:
-                # print m
-                j = (m*m - 3)//2
-                # print j
-                # print s[j]
+    i = 0
+    m = 3
+    # print 's:', s
+    while m <= mroot:
+        if s[i]:
+            # print m
+            j = (m*m - 3)//2
+            # print j
+            # print s[j]
+            s[j] = 0
+            j += m
+            while j < half:
                 s[j] = 0
-                j += m
-                while j < half:
-                    s[j] = 0
-                        j += m
+                    j += m
 
-                i += 1
-                m = 2*i+3
+            i += 1
+            m = 2*i+3
 
-        # print [2] + [x for x in s if x]
-        print sum(s) + 2
-        end = time.time()
-        print end - start
+    # print [2] + [x for x in s if x]
+    print sum(s) + 2
+    end = time.time()
+    print end - start
