3cc204cf59f55e6501e899fadbab4d71a85db21b
leafsummer <wing.sun1987@gmail.com>
2016-04-15 16:53:54 +0800

    update md2pdf

--- markdown2pdf/md2pdf.py
+++ markdown2pdf/md2pdf.py
@@ -6,22 +6,32 @@ from weasyprint import HTML
 from markdown2 import markdown_path
 
 
-def convert_md_2_pdf(filepath, output=None, theme=None):
-    html = markdown_path(filepath)
+def convert_md_2_pdf(filepath, output=None, theme=None， codecss=None):
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    html = markdown_path(filepath, extras=["code-friendly", "fenced-code-blocks"])
+    css_file_list = []
     if output and os.path.isdir(output):
         output = '.'.join([output, filepath.rsplit('.')[0], 'pdf'])
     else:
         output = '.'.join([filepath.rsplit('.')[0], 'pdf'])
-
-    print 'output file:', output
     if theme is not None:
-        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
         css_file = theme
         if not os.path.exists(css_file):
             css_file = os.path.join(BASE_DIR, 'themes/'+theme+'.css')
+        print 'theme_css:', css_file
+        css_file_list.append(css_file)
+        # HTML(string=html).write_pdf(output, stylesheets=[css_file])
+    
+    if codecss is not None:
+        css_file = codecss
+        if not os.path.exists(css_file):
+            css_file = os.path.join(BASE_DIR, 'pygments-css/'+codecss+'.css')
+        print 'code_css:', css_file
+        css_file_list.append(css_file)
 
-        print 'css:',css_file
-        HTML(string=html).write_pdf(output, stylesheets=[css_file])
+    print 'output file:', output
+    if css_file_list:
+        HTML(string=html).write_pdf(output, stylesheets=css_file_list)
     else:
         HTML(string=html).write_pdf(output)
 
@@ -30,6 +40,7 @@ def main():
     parser = argparse.ArgumentParser(description='Convert markdown file to pdf')
     parser.add_argument('-f', '--filepath', help='Markdown file path')
     parser.add_argument('-t', '--theme', help='Set the theme, default is GitHub flavored.', default='github')
+    parser.add_argument('-cc', '--codecss', help='Set the code css, default is pygments friendly css.', default='friendly')
     parser.add_argument('-o', '--output', help='The output file path. If not set, '
                         'the name will be same as the input file but with ".pdf" in current dir.')
     args = parser.parse_args()
