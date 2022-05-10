9de5ddda4a7ee570ed0e2f70a8094e4ac85fb544
邬庆峰 <wuqingfeng@intra.nsfocus.com>
2022-05-10 19:33:10 +0800

    å¢å ä»£ç æ ¡éªåèªå¨è¡¥å¨çæä»¶

---
+++ simple_vim/nvim_after_config/plugin/nvim-cmp.lua
@@ -0,0 +1,96 @@
+local lspkind = require('lspkind')
+local cmp = require'cmp'
+
+cmp.setup {
+  -- 指定 snippet 引擎
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` users.
+      vim.fn["vsnip#anonymous"](args.body)
+
+      -- For `luasnip` users.
+      -- require('luasnip').lsp_expand(args.body)
+
+      -- For `ultisnips` users.
+      -- vim.fn["UltiSnips#Anon"](args.body)
+
+      -- For `snippy` users.
+      -- require'snippy'.expand_snippet(args.body)
+    end,
+  },
+  -- 来源
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- For vsnip users.
+    { name = 'vsnip' },
+    -- For luasnip users.
+    -- { name = 'luasnip' },
+    --For ultisnips users.
+    -- { name = 'ultisnips' },
+    -- -- For snippy users.
+    -- { name = 'snippy' },
+  }, { { name = 'buffer' },
+       { name = 'path' }
+    }),
+
+  -- 快捷键
+  -- mapping = require('keybindings').cmp(cmp),
+  -- 使用lspkind-nvim显示类型图标
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true, -- do not show text alongside icons
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      before = function (entry, vim_item)
+        -- Source 显示提示来源
+        vim_item.menu = "["..string.upper(entry.source.name).."]"
+        return vim_item
+      end
+    })
+  },
+  mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false
+    },
+	  ['<C-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+	  ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end
+  }
+}
+
+-- Use buffer source for `/`.
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+      { name = 'cmdline' }
+    })
+})
---
+++ simple_vim/nvim_after_config/plugin/nvim-lsp-installer.lua
@@ -0,0 +1,19 @@
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+    local opts = { noremap=true, silent=true }
+    vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function is exactly the same as lspconfig's setup function.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
